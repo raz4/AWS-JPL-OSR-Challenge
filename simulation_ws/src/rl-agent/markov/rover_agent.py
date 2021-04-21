070c77a76258659a87a60cc111af3d230cbb06a8
Muslim Razi <razi.muslim@gmail.com>
2021-04-20 19:03:08 -0700

    remove S3 snippets

--- simulation_ws/src/rl-agent/markov/rover_agent.py
+++ simulation_ws/src/rl-agent/markov/rover_agent.py
@@ -89,23 +89,11 @@ def main():
 
     # TODO: support other frameworks
     task_parameters = TaskParameters(framework_type=Frameworks.tensorflow,
-                                     checkpoint_save_secs=args.checkpoint_save_secs)
+                                     checkpoint_save_secs=args.checkpoint_save_secs,
+                                     experiment_path='./logs')
     task_parameters.__dict__['checkpoint_save_dir'] = args.local_model_directory
     task_parameters.__dict__ = add_items_to_dict(task_parameters.__dict__, args.__dict__)
 
-    data_store_params_instance = S3BotoDataStoreParameters(bucket_name=args.model_s3_bucket,
-                                                           s3_folder=args.model_s3_prefix,
-                                                           checkpoint_dir=args.local_model_directory,
-                                                           aws_region=args.aws_region)
-
-
-    data_store = S3BotoDataStore(data_store_params_instance)
-
-    if args.save_frozen_graph:
-        data_store.graph_manager = graph_manager
-
-    graph_manager.data_store_params = data_store_params_instance
-    graph_manager.data_store = data_store
     graph_manager.should_stop = should_stop_training_based_on_evaluation
     start_graph(graph_manager=graph_manager, task_parameters=task_parameters)
 
